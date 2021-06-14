dccb5f57e90258ef5c07e8082a984f857a096bc1
mandarin4452 <ckdgns0130@gmail.com>
2021-06-14 18:09:42 +0900

    Add finish or not function

--- src/todo.js
+++ src/todo.js
@@ -2,6 +2,7 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 const savedTodo = localStorage.getItem("todos");
+
 let toDos = [];
 
 function saveTodo() {
@@ -15,20 +16,45 @@ function deleteTodo(event) {
     saveTodo();
 }
 
+function finishTodo(event) {
+    const li = event.target.parentElement;
+    toDos.forEach(toDo => {
+        if (toDo.id === parseInt(li.id) && toDo.finish === 0) {
+            li.style.textDecoration = "line-through";
+            li.style.color = "grey";
+            toDo.finish = 1;
+        }
+        else if (toDo.id === parseInt(li.id) && toDo.finish === 1) {
+            li.style.textDecoration = "";
+            li.style.color = "white";
+            toDo.finish = 0;
+        }
+    })
+
+    
+    saveTodo();
+
+}
+
 function paintTodo(newTodo) {
     const li = document.createElement("li");
     li.id = newTodo.id;
 
     const span = document.createElement("span");
-    //const finishButton = document.createElement("button");
+    const finishButton = document.createElement("button");
     const delButton = document.createElement("button");
-    //finishButton.innerHTML = '&#10004;';
+    finishButton.innerHTML = '&#10004;';
     delButton.innerHTML = '&#10006;';
     delButton.addEventListener("click",deleteTodo);
-    //li.appendChild(finishButton);
+    finishButton.addEventListener("click",finishTodo);
+    li.appendChild(finishButton);
     li.appendChild(span);
     li.appendChild(delButton);
     span.innerText = newTodo.text;
+    if (newTodo.finish === 1) {
+        li.style.textDecoration = "line-through";
+        li.style.color = "grey";
+    }
     todoList.appendChild(li);
 }
 
@@ -39,6 +65,7 @@ function handleTodo(event) {
     const newTodoObj = {
         id : Date.now(),
         text : newTodo,
+        finish : 0,
     };
     toDos.push(newTodoObj);
     paintTodo(newTodoObj);
